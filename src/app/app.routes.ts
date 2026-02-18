a180434422717eff547a238e08759e59826a058e
Kolton Bloom <bloomkd46@icloud.com>
2026-02-18 09:01:05 -0800

    update migrations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) }
+  { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+  { path: 'skis', loadComponent: () => import('./pages/skis/skis.component').then(m => m.SkisComponent) },
 ];
