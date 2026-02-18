0601c0bc26ba2d0178d51c8ad0b8eeb934097397
Kolton Bloom <bloomkd46@icloud.com>
2026-02-18 09:14:58 -0800

    fixed rendering

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
 
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,7 @@
-import { provideZonelessChangeDetection } from "@angular/core";
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, { ...config, providers: [provideZonelessChangeDetection(), ...config.providers] }, context);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, { ...config, providers: [...config.providers] }, context);
 
 export default bootstrap;
