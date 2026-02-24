b39af0c31599e1f648572b145615408967452af5
Kolton Bloom <bloomkd46@icloud.com>
2026-02-24 08:28:57 -0800

    added 404 page to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
   { path: 'skis', loadComponent: () => import('./pages/skis/skis.component').then(m => m.SkisComponent) },
+  { path: '404', loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent), title: 'Page Not Found' },
   { path: '**', loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent), title: 'Page Not Found' }
 ];
