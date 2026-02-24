bfedee3fdfd6990fa956331e16c8664a07cad1e2
Kolton Bloom <bloomkd46@icloud.com>
2026-02-24 08:19:28 -0800

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,5 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
   { path: 'skis', loadComponent: () => import('./pages/skis/skis.component').then(m => m.SkisComponent) },
+  { path: '**', loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent), title: 'Page Not Found' }
 ];
